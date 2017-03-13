1d7cee521938be760e3701cfe6ad848b9f33c5f5
ilhamwara <ilhamwara@gmail.com>
2017-03-13 12:03:50 +0700

    as

---
+++ app/ReportAssessment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReportAssessment extends Model
+{
+    protected $table = 'report_assesment';
+}
--- resources/views/assesment/lembar.blade.php
+++ resources/views/assesment/lembar.blade.php
@@ -39,99 +39,93 @@
 			</table>
 		</div>
 		<div class="text-right">
-			<a href="{{url('arsip/assessment')}}" class="btn btn-default" data-toggle="tooltip" title="Lihat Arsip Assessment">Arsip</a>
+			<a href="{{url('arsip/assessment')}}" class="btn btn-lg btn-default" data-toggle="tooltip" title="Lihat Arsip Assessment"><i class="fa fa-book"></i> Arsip</a>
 		</div>
 		<br>
 		<div class="table-responsive">
-			<div id="dataTables-example_wrapper" class="dataTables_wrapper form-inline" role="grid">
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="dataTables_length" id="dataTables-example_length">
-							<label>
-								<select name="dataTables-example_length" aria-controls="dataTables-example" class="form-control input-sm">
-									<option value="10">10</option>
-									<option value="25">25</option>
-									<option value="50">50</option>
-									<option value="100">100</option>
-								</select>
-								records per page
-							</label>
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<div id="dataTables-example_filter" class="dataTables_filter">
-							<label>
-								Search:<input type="search" class="form-control input-sm" aria-controls="dataTables-example">
-							</label>
-						</div>
-					</div>
-				</div>
-				<table class="table table-striped table-bordered table-hover dataTable no-footer" id="dataTables-example" aria-describedby="dataTables-example_info">
-					<thead>
-						<tr role="row">
-							<th style="width: 113px;" class="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Bulan/Periode: activate to sort column ascending">
-								Bulan/Periode
-							</th>
-							<th style="width: 83px;" class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Tahun: activate to sort column ascending">
-								Tahun
-							</th>
-							<th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Progress: activate to sort column ascending" style="width: 471px;">
-								Progress
-							</th>
-							<th class="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Pengaturan: activate to sort column ascending" style="width: 273px;">
-								Pengaturan
-							</th>
-						</tr>
-					</thead>
-					<tbody>
+			<table class="table table-striped table-bordered table-hover dataTable no-footer" id="myTable" aria-describedby="dataTables-example_info">
+				<thead>
+					<tr role="row">
+						<th>Bulan/Periode</th>
+						<th>Tahun</th>
+						<th>Progress</th>
+						<th>Pengaturan</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$datenya = date('m');
+					if (date('m') == '1' || date('m') == '2' || date('m') == '3') {
+						$tri = '1';
+					}
+					elseif(date('m') == '4' || date('m') == '5' || date('m') == '6') {
+						$tri = '2';
+					}
+					elseif(date('m') == '7' || date('m') == '8' || date('m') == '9') {
+						$tri = '3';
+					}
+					elseif(date('m') == '10' || date('m') == '11' || date('m') == '12') {
+						$tri = '4';
+					}
 
-						<tr class="odd">
-							<td class="text-center">Januari - Maret</td>
-							<td class="text-center">2017</td>
-							<td >
-								<div>
-									<p>
-										<strong>Terakhir diubah :</strong> 20-12-2019
-										<span class="text-muted pull-right">20% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-											<span class="sr-only">20% Complete</span>
-										</div>
+					$report = \App\ReportAssessment::where('user_id','1')->where('tahun',date('Y'))->where('triwulan',$tri)->orderBy('created_at','DESC')->get();
+
+					?>
+					@forelse($report as $data)
+					<?php
+					if($data->triwulan == '1') {
+						$triwulan = 'Januari - Maret';
+					}
+					elseif($data->triwulan == '2'){
+						$triwulan = 'April - Juni';
+					}
+					elseif($data->triwulan == '3'){
+						$triwulan = 'Juli - September';
+					}
+					elseif($data->triwulan == '4'){
+						$triwulan = 'Oktober - Desember';
+					}
+					?>
+					<tr class="odd">
+						<td class="text-center">{{$triwulan}}</td>
+						<td class="text-center">{{$data->tahun}}</td>
+						<td >
+							<div>
+
+								<p>
+									<strong>Terakhir diubah :</strong> 20-12-2019
+									<span class="text-muted pull-right">20% Complete</span>
+								</p>
+								<div class="progress progress-striped active">
+									<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+										<span class="sr-only">20% Complete</span>
 									</div>
 								</div>
-							</td>
-							<td class="text-center">
-							<br>
-								<a href="{{url('edit-self-assessment')}}" class="btn btn-warning">Ubah 
-								</a>
-							</td>
-						</tr>
-						<tr class="odd">
-							<td class="text-center">Januari - Maret</td>
-							<td class="text-center">2017</td>
-							<td >
-								<div>
-									<p>
-										<strong>Final pada :</strong> 20-12-2019
-										<span class="text-muted pull-right">100% Complete</span>
-									</p>
-									<div class="progress progress-striped">
-										<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-											<span class="sr-only">100% Complete</span>
-										</div>
+
+								<strong>Final pada :</strong> 20-12-2019
+								<span class="text-muted pull-right">100% Complete</span>
+								<div class="progress progress-striped">
+									<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+										<span class="sr-only">100% Complete</span>
 									</div>
 								</div>
-							</td>
-							<td class="text-center">
+
+							</div>
+						</td>
+						<td class="text-center">
 							<br>
-								<a href="{{url('detail/assessment')}}" class="btn btn-primary">Preview </a>
-								<span class="btn btn-success"> Sudah di Final</span>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+							<a href="{{url('edit-self-assessment')}}" class="btn btn-warning">Ubah 
+							</a>
+							<a href="{{url('detail/assessment')}}" class="btn btn-primary">Preview </a>
+							@if($data->final_status == 1)
+							<span class="btn btn-success"> Sudah di Final</span>
+							@endif
+						</td>
+					</tr>
+					@empty
+					@endforelse
+				</tbody>
+			</table>
 		</div>
 		<!-- END Datatables Content -->
 	</div>
@@ -141,4 +135,9 @@
 	@section('js')
 	<script src="{{asset('vendor/js/pages/tablesDatatables.js')}}"></script>
 	<script>$(function(){ TablesDatatables.init(); });</script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#myTable').DataTable();
+		});
+	</script>
 	@endsection
