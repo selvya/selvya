b63872f546d47245a8e4e745bbfdabdbe1f07232
PetengDedet <puetengdedet@gmail.com>
2017-03-14 13:56:33 +0700

    yy

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -2,11 +2,11 @@
 use Carbon\Carbon;
 
 function getSatker(){
-    // if (Auth::check()) {
-    //     return Auth::user()->id;
-    // }
+    if (Auth::check() AND Auth::user()->level == 'satker') {
+        return Auth::user()->id;
+    }
 
-    return 8;
+    return false;
 }
 
 function getBatasTanggalPelaporan($tahun = null, $triwulan = 1) {
@@ -85,4 +85,89 @@ function cekIkuSerapan($tahun = null, $triwulan = null) {
 
 function getPercentOfNumber($number, $percent){
     return ($percent / 100) * $number;
+}
+
+
+
+//CREATE AKUN TOKEN
+function createToken($id, $level, $username) {
+    if ($id == null OR $level == null OR $username == null) {
+        return false;
+    }
+
+    $user = User::
+    $secret = env('SKRT_KEY', 'rj"|:"<>HJFhgcdU857641923RYFGHolfs346346xckjhrw243rhYRjyf^%mnY%$#~()=-=-$%&~!@!@xvxjfw%$#!%@$&^@#d+');
+    return $secret;
+    // $user = \App\User::
+}
+
+function cek($token = null)
+{
+    /*|-------------------------------------------------
+     *|!!!!!! Variable $secret JANGAN DIGANTI!!!!!
+     *|-------------------------------------------------*/
+    $secret = env('SKRT_KEY', 'rj"|:"<>HJFhgcdU857641923RYFGHolfs346346xckjhrw243rhYRjyf^%mnY%$#~()=-=-$%&~!@!@xvxjfw%$#!%@$&^@#d+');
+    // $secret = 'token.rahasia..';
+    $data = [
+        'status' => false,
+        'data'  => '',
+        'message' => ''
+    ];
+    
+    if ($token == null) {
+        $data['message'] = 'token kosong. ulangi';
+        die(json_encode($data));
+    }
+
+    if (Hash::check($secret, $token)){
+        $token = \App\ApiToken::where('token', $token)->get();
+
+        if (count($token) <= 0) {
+            $data['message'] = 'Token Invalid or Expired';
+            die(json_encode($data));
+        }
+
+        $user = \App\User::where('username', $token->first()->username)->first();
+
+        $data['status'] = true;
+        $data['data'] = $user;
+        $data['token'] = $token->first()->token;
+
+        return $data;
+    }
+    
+    $data['message'] = 'Token Salah Ulangi Kembali';
+
+    die(json_encode($data));
+}
+
+function gen($username, $level)
+{
+    /*|-------------------------------------------------
+     *|!!!!!! Variable $secret JANGAN DIGANTI!!!!!
+     *|-------------------------------------------------*/
+    $secret = env('SKRT_KEY', 'rj"|:"<>HJFhgcdU857641923RYFGHolfs346346xckjhrw243rhYRjyf^%mnY%$#~()=-=-$%&~!@!@xvxjfw%$#!%@$&^@#d+');
+
+
+    $api_token = new \App\ApiToken();
+
+    $token = Hash::make($secret);
+    $api_token->username = $username;
+    $api_token->token = $token;
+
+    $user = \App\User::updateOrCreate(
+        [
+            'username' => $username,
+            'password' => null
+        ],
+        [
+            'level' => $level
+        ]
+    );
+
+    if($api_token->save()) {
+        return ['token' => $api_token->token];
+    }
+
+    return 'Token Salah Ulangi Kembali';
 }
