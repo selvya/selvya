9e705f99e912c0218fc75235e1549a5117399f5b
ilhamwara <ilhamwara@gmail.com>
2017-03-21 17:17:51 +0700

    as

--- resources/views/assesment/pimpinan.blade.php
+++ resources/views/assesment/pimpinan.blade.php
@@ -53,149 +53,156 @@
 
                     @include('include.alert')
                     @php
+
+                    $rep = \App\ReportAssessment::where('tahun', date('Y'))
+                    ->where('triwulan', cekCurrentTriwulan()['current']->triwulan)
+                    ->where('daftarindikator_id', 1)
+                    ->where('user_id', getSatker())
+                    ->first();
+
                     $triwulan = cekCurrentTriwulan();
                     $reportall = \App\ReportAssessment::where('triwulan',$triwulan['current']['triwulan'])
                     ->where('tahun',date('Y'))
                     ->where('user_id',Auth::user()->id)
                     ->where('daftarindikator_id','3')
+                    ->get();
+
+
+                    $agg = \App\AnggaranTahun::where('tahun', date('Y'))
+                    ->where('user_id', getSatker())
+                    ->first()
+                    ->anggaran_triwulan
+                    ->where('triwulan', cekCurrentTriwulan()['current']->triwulan)
                     ->first();
-                    @endphp
-                    <div class="form-group">
-                        <div class="col-xs-12">
-                            <ul class="nav nav-pills nav-justified clickable-steps">
-                                @if(($inovatif != null ) || ($melayani != null) || ($peduli != null))
-                                <li class="@if(!cekBudaya(date('Y'), $triwulan['current']['triwulan'], Auth::user()->id)) red @else hijau @endif">
-                                    <a href="{{url('edit-self-assessment/'.$reportall->last()->hashid.'/programbudaya')}}" data-gotostep="clickable-first">
-                                        <strong><i class="fa fa-check"></i>Pelaksanaan Program Budaya <br> 
-                                            <big>{{$reportall->last()->hasil}}%</big> <big>[{{$persen->nilai}}%]</big>
-                                        </a>
-                                    </li>
-                                    @endif
-                                    @if($anggaran != null)
-                                    <li class="@if($atasWizard == 0) red @else hijau @endif">
-                                        @php
+                    if ($agg->is_final != 0) {
+                    $atasWizard = (hitungNilaiSerapan(date('Y'), cekCurrentTriwulan()['current']->triwulan, Auth::user()->id) / 6) * cekPersenSerapan(date('Y'), 2, cekCurrentTriwulan()['current']->triwulan)->nilai;
+                }
+                else{
+                $atasWizard = 0;
+            }
 
-                                        $agg = \App\AnggaranTahun::where('tahun', date('Y'))
-                                        ->where('user_id', getSatker())
-                                        ->first()
-                                        ->anggaran_triwulan
-                                        ->where('triwulan', cekCurrentTriwulan()['current']->triwulan)
-                                        ->first();
-                                        if ($agg->is_final != 0) {
-                                        $atasWizard = (hitungNilaiSerapan(date('Y'), cekCurrentTriwulan()['current']->triwulan, Auth::user()->id) / 6) * cekPersenSerapan(date('Y'), 2, cekCurrentTriwulan()['current']->triwulan)->nilai;
-                                    }else{
-                                    $atasWizard = 0;
-                                }
+            $pim = cekFinalPimpinan(date('Y'), cekCurrentTriwulan()['current']->triwulan, getSatker());
+            $nilaiPim = cekNilaiPimpinan(date('Y'), cekCurrentTriwulan()['current']->triwulan, getSatker());
 
-                                $pim = cekFinalPimpinan(date('Y'), cekCurrentTriwulan()['current']->triwulan, getSatker());
 
-                                @endphp
-                                <a href="{{url('edit-self-assessment/'.Request::segment(2).'/serapan-anggaran')}}" data-gotostep="clickable-second" class="stepnya"><strong>
-                                    <i class="fa fa-check"></i>Serapan Anggaran <br> <big>{{$atasWizard}}% [{{$anggaran->nilai}}%]</big></strong>
-                                </a>
-                            </li>
-                            @endif
-                            @if($pimpinan != null)
-                            @php
-                            $nilaiPim = cekNilaiPimpinan(date('Y'), cekCurrentTriwulan()['current']->triwulan, getSatker());
-                            @endphp
-                            <li class="@if($nilaiPim == 0) red @else hijau @endif">
-                                <a href="{{url('edit-self-assessment/'.Request::segment(2).'/partisipasi-pimpinan')}}" data-gotostep="clickable-third">
-                                    <strong>Partisipan Pimpinan <br> <big>{{$nilaiPim}}% [{{$pimpinan->nilai}}%]</big></strong>
-                                </a>
-                            </li>
-                            @endif
-                            @if($pelaporan != null)
-                            <li class="@if(( ((int) cekSimpanPelaporan($rep)) / 6) * cekPersenLaporan(date('Y'), 1, cekCurrentTriwulan()['current']->triwulan)->nilai == 0) red @else hijau @endif">
-                                <a href="{{url('edit-self-assessment/'.Request::segment(2).'/kecepatan-pelaporan')}}" data-gotostep="clickable-fourth">
-                                    <strong>Kecepatan Pelaporan <br> <big>{{$pelaporan->nilai}}%</big></strong>
-                                </a>
-                            </li>
-                            @endif
-                        </ul>
-                    </div>
-                </div>
-                <br>
-                <div class="container" style="max-width: 1000px; overflow: hidden;">
-                    <div class="block">
-                        @php
-                        $persen = \App\Persentase::where('tahun', date('Y'))
-                        ->where('triwulan', cekCurrentTriwulan()['current']->triwulan)
-                        ->where('daftarindikator_id', 4)
-                        ->first();
-
-                        $iku = \App\Iku::with('alat_ukur.definisi')
-                        ->where(
-                        'namaprogram',
-                        'partisipasi_pimpinan#' .
-                        date('Y') . '#' .
-                        cekCurrentTriwulan()['current']->triwulan
-                        )->first();
-                        // dd($iku);
-                        @endphp
-
-                        @if(!$pim)
-                        <form class="form-horizontal" method="POST" action="">
-
-                            @if($iku->tipe == 'parameterized')
-                            <div class="form-group">
-                                <label class="control-label col-lg-3">Nilai</label>
-                                <div class="col-md-6">
-                                    <select name="nilai" class="form-control" required> 
-                                        @foreach($iku->alat_ukur->first()->definisi as $k => $v)
-                                        <option value="{{$k+1}}">{{$k+1}} - {{$v->deskripsi}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            @else
-                            <div class="form-group">
-                                <label class="control-label col-lg-3">Nilai</label>
-                                <div class="col-md-6">
-                                    <input type="text" name="nilai" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-lg-3">Partisipasi Pimpinan</label>
-                                <div class="col-md-6">
-                                    <textarea name="partisipasi" class="form-control"></textarea>
-                                </div>
-                            </div>
-                            @endif
-
-                            <div class="form-group">
-                                <div class="col-md-3">
-                                    {{csrf_field()}}
-                                </div>
-                                <div class="col-md-6">
-                                    <button type="submit" class="btn btn-primary pull-right">Simpan <i class="fa fa-save"></i></button>
-                                </div>
-                            </div>
-                        </form>
-
-                        @else
-                        <div class="jumbotron">
-                            @php
-                            $ppp = \App\ReportAssessment::where('tahun', date('Y'))
-                            ->where('triwulan', cekCurrentTriwulan()['current']->triwulan)
-                            ->where('daftarindikator_id', 4)
-                            ->where('user_id', getSatker())
-                            ->first();
-                            @endphp
-
-                            <h4>Nilai: {{$ppp->nilai}} ({{cekNilaiPimpinan(date('Y'), cekCurrentTriwulan()['current']->triwulan, getSatker())}}%)</h4>
-                            <p>{{$ppp->keterangan}}</p>
-                        </div>
-                        @endif            
-                    </div>
+            @endphp
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <ul class="nav nav-pills nav-justified clickable-steps">
+                        @if(($inovatif != null ) || ($melayani != null) || ($peduli != null))
+                        <li class="@if(!cekBudaya(date('Y'), $triwulan['current']['triwulan'], Auth::user()->id)) red @else hijau @endif">
+                            <a href="{{url('edit-self-assessment/'.$reportall->last()->hashid.'/programbudaya')}}" data-gotostep="clickable-first">
+                                <strong><i class="fa fa-check"></i>Pelaksanaan Program Budaya <br> 
+                                    <big>{{$reportall->last()->hasil}}%</big> <big>[{{$persen->nilai}}%]</big>
+                                </strong>
+                            </a>
+                        </li>
+                        @endif
+                        @if($anggaran != null)
+                        <li class="@if($atasWizard == 0) red @else hijau @endif">
+                            <a href="{{url('edit-self-assessment/'.Request::segment(2).'/serapan-anggaran')}}" data-gotostep="clickable-second" class="stepnya"><strong>
+                                <i class="fa fa-check"></i>Serapan Anggaran <br> <big>{{$atasWizard}}% [{{$anggaran->nilai}}%]</big></strong>
+                            </a>
+                        </li>
+                        @endif
+                        @if($pimpinan != null)
+                        <li class="@if($nilaiPim == 0) red @else hijau @endif">
+                            <a href="{{url('edit-self-assessment/'.Request::segment(2).'/partisipasi-pimpinan')}}" data-gotostep="clickable-third">
+                                <strong>Partisipan Pimpinan <br> <big>{{$nilaiPim}}% [{{$pimpinan->nilai}}%]</big></strong>
+                            </a>
+                        </li>
+                        @endif
+                        @if($pelaporan != null)
+                        <li class="@if(( ((int) cekSimpanPelaporan($rep)) / 6) * cekPersenLaporan(date('Y'), 1, cekCurrentTriwulan()['current']->triwulan)->nilai == 0) red @else hijau @endif">
+                            <a href="{{url('edit-self-assessment/'.Request::segment(2).'/kecepatan-pelaporan')}}" data-gotostep="clickable-fourth">
+                                <strong>Kecepatan Pelaporan <br> <big>{{$pelaporan->nilai}}%</big></strong>
+                            </a>
+                        </li>
+                        @endif
+                    </ul>
                 </div>
             </div>
-            <!-- END Third Step -->
-            <!-- END Form Buttons -->
-        </form>
-        <!-- END Wizard with Validation Content -->
-    </div>
-    <!-- END Wizard with Validation Block -->
+            <br>
+            <div class="container" style="max-width: 1000px; overflow: hidden;">
+                <div class="block">
+                    @php
+                    $persen = \App\Persentase::where('tahun', date('Y'))
+                    ->where('triwulan', cekCurrentTriwulan()['current']->triwulan)
+                    ->where('daftarindikator_id', 4)
+                    ->first();
+
+                    $iku = \App\Iku::with('alat_ukur.definisi')
+                    ->where(
+                    'namaprogram',
+                    'partisipasi_pimpinan#' .
+                    date('Y') . '#' .
+                    cekCurrentTriwulan()['current']->triwulan
+                    )->first();
+                    // dd($iku);
+                    @endphp
+
+                    @if(!$pim)
+                    <form class="form-horizontal" method="POST" action="">
+
+                        @if($iku->tipe == 'parameterized')
+                        <div class="form-group">
+                            <label class="control-label col-lg-3">Nilai</label>
+                            <div class="col-md-6">
+                                <select name="nilai" class="form-control" required> 
+                                    @foreach($iku->alat_ukur->first()->definisi as $k => $v)
+                                    <option value="{{$k+1}}">{{$k+1}} - {{$v->deskripsi}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        @else
+                        <div class="form-group">
+                            <label class="control-label col-lg-3">Nilai</label>
+                            <div class="col-md-6">
+                                <input type="text" name="nilai" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-lg-3">Partisipasi Pimpinan</label>
+                            <div class="col-md-6">
+                                <textarea name="partisipasi" class="form-control"></textarea>
+                            </div>
+                        </div>
+                        @endif
+
+                        <div class="form-group">
+                            <div class="col-md-3">
+                                {{csrf_field()}}
+                            </div>
+                            <div class="col-md-6">
+                                <button type="submit" class="btn btn-primary pull-right">Simpan <i class="fa fa-save"></i></button>
+                            </div>
+                        </div>
+                    </form>
+
+                    @else
+                    <div class="jumbotron">
+                        @php
+                        $ppp = \App\ReportAssessment::where('tahun', date('Y'))
+                        ->where('triwulan', cekCurrentTriwulan()['current']->triwulan)
+                        ->where('daftarindikator_id', 4)
+                        ->where('user_id', getSatker())
+                        ->first();
+                        @endphp
+
+                        <h4>Nilai: {{$ppp->nilai}} ({{cekNilaiPimpinan(date('Y'), cekCurrentTriwulan()['current']->triwulan, getSatker())}}%)</h4>
+                        <p>{{$ppp->keterangan}}</p>
+                    </div>
+                    @endif            
+                </div>
+            </div>
+        </div>
+        <!-- END Third Step -->
+        <!-- END Form Buttons -->
+    </form>
+    <!-- END Wizard with Validation Content -->
+</div>
+<!-- END Wizard with Validation Block -->
 </div>
 </div>
 <!-- END Wizards Row -->    
--- resources/views/assesment/rekap.blade.php
+++ resources/views/assesment/rekap.blade.php
@@ -2,101 +2,107 @@
 @section('content')
 <!-- Page content -->
 <div id="page-content">
-	<!-- Datatables Header -->
-	<div class="content-header">
-		<div class="header-section">
-			<h1>
-				<i class="gi gi-sort"></i>
-				<b>Rekap Assessment</b>
-			</h1>
-		</div>
-	</div>
-	<ul class="breadcrumb breadcrumb-top">
-		<li><a href="{{url('/')}}">Beranda</a></li>
-		<li>Rekap Assessment</li>
-	</ul>
-	<!-- END Datatables Header -->
+    <!-- Datatables Header -->
+    <div class="content-header">
+        <div class="header-section">
+            <h1>
+                <i class="gi gi-sort"></i>
+                <b>Rekap Assessment</b>
+            </h1>
+        </div>
+    </div>
+    <ul class="breadcrumb breadcrumb-top">
+        <li><a href="{{url('/')}}">Beranda</a></li>
+        <li>Rekap Assessment</li>
+    </ul>
+    <!-- END Datatables Header -->
 
-	<!-- Datatables Content -->
-	<div class="block full">
-		<div class="text-right">
-			<a href="{{url('arsip/assessment')}}" class="btn btn-default" data-toggle="tooltip" title="Lihat Arsip Assessment"><i class="fa fa-eye"></i></a>
-		</div>
-		<br>
-		
-<!-- 			<div class="panel-heading">
-				<form method="post" enctype="multipart/form-data" action="">
-					<select name="tipe">
-						<option value="2" selected="selected">Kantor Pusat</option>
-						<option value="3">Kantor Regional</option>
-						<option value="4">Kantor KOJK</option>
-						<option value="5">OJK-wide</option>
-					</select>&nbsp;							Periode&nbsp;:&nbsp;&nbsp;
-					<select name="month">
-						<option value="13" selected="selected">Januari - Maret</option>
-						<option value="14">April - Juni</option>
-						<option value="15">Juli - September</option>
-						<option value="16">Oktober-Desember</option>
-					</select>&nbsp;<select name="year">
-					<option value="2017" selected="selected">2017</option>
-					<option value="2016">2016</option>
-					<option value="2015">2015</option>
-				</select>&nbsp;			
-				<a class="btn btn-primary">Lihat&nbsp;<i class="fa fa-arrow-circle-o-right"></i></a>
-			</form>
-		</div>
-		<div class="panel-body" style="padding:0px; margin-top:5px; margin-left:-1px;">
-			<div class="container" style=" width:100%;margin-bottom: 5px;">
-				<b>Detail Rekapitulasi:</b>
-				<table>
-					<tr>
-						<td style="background-color: #ececec;" align="center">
-							&nbsp;Sudah Submit&nbsp;<br>
-							<a href="#" class="btn btn-success">0</a>
-						</td>
-						<td>&nbsp;</td>
-						<td style="background-color: #ececec;" align="center">
-							&nbsp;Belum Submit&nbsp;<br>
-							<a href="#" class="btn btn-danger">66</a>
-						</td>
-					</tr>
-				</table>
-			</div>
-		</div> -->
-		
-		<table class="table table-striped table-bordered table-hover" id="dataTables-example">
-			<thead>
-				<tr>
-					<th>Deputi Komisioner</th>
-					<th>Departemen</th>
-					<th>KOJK</th>
-					<th>Progress</th>
-				</tr>
-			</thead>
-			<?php
-			$triwulan = cekCurrentTriwulan();
-			$report = \App\ReportAssessment::all();
-			$satker =  \App\User::
-			join('report_assesment','users.id','=','report_assesment.user_id')
-			->whereNotIn('users.id',[$report])
-			->get();
-			// dd($satker);
+    <!-- Datatables Content -->
+    <div class="block full">
+        <div class="text-right">
+            <a href="{{url('arsip/assessment')}}" class="btn btn-default" data-toggle="tooltip" title="Lihat Arsip Assessment"><i class="fa fa-eye"></i></a>
+        </div>
+        <br>
+        
+<!--            <div class="panel-heading">
+                <form method="post" enctype="multipart/form-data" action="">
+                    <select name="tipe">
+                        <option value="2" selected="selected">Kantor Pusat</option>
+                        <option value="3">Kantor Regional</option>
+                        <option value="4">Kantor KOJK</option>
+                        <option value="5">OJK-wide</option>
+                    </select>&nbsp;                         Periode&nbsp;:&nbsp;&nbsp;
+                    <select name="month">
+                        <option value="13" selected="selected">Januari - Maret</option>
+                        <option value="14">April - Juni</option>
+                        <option value="15">Juli - September</option>
+                        <option value="16">Oktober-Desember</option>
+                    </select>&nbsp;<select name="year">
+                    <option value="2017" selected="selected">2017</option>
+                    <option value="2016">2016</option>
+                    <option value="2015">2015</option>
+                </select>&nbsp;         
+                <a class="btn btn-primary">Lihat&nbsp;<i class="fa fa-arrow-circle-o-right"></i></a>
+            </form>
+        </div>
+        <div class="panel-body" style="padding:0px; margin-top:5px; margin-left:-1px;">
+            <div class="container" style=" width:100%;margin-bottom: 5px;">
+                <b>Detail Rekapitulasi:</b>
+                <table>
+                    <tr>
+                        <td style="background-color: #ececec;" align="center">
+                            &nbsp;Sudah Submit&nbsp;<br>
+                            <a href="#" class="btn btn-success">0</a>
+                        </td>
+                        <td>&nbsp;</td>
+                        <td style="background-color: #ececec;" align="center">
+                            &nbsp;Belum Submit&nbsp;<br>
+                            <a href="#" class="btn btn-danger">66</a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div> -->
+        
+        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+            <thead>
+                <tr>
+                    <th class="text-center">Username</th>
+                    <th class="text-center">Deputi Komisioner</th>
+                    <th class="text-center">Departemen</th>
+                    <th class="text-center">KOJK</th>
+                    <th class="text-center">Progress</th>
+                </tr>
+            </thead>
+            <?php
+            $triwulan = cekCurrentTriwulan();
+            $report = \App\ReportAssessment::where('tahun',date('Y'))
+                      ->where('triwulan',$triwulan['current']['triwulan'])->get();
+            $rp = [];
+            foreach ($report as $key => $value) {
+               $rp[] = $value->user_id;
+           }
+           $satker =  \App\User::whereNotIn('id',$rp)
+           ->paginate(15);
 
-			
-			?>
-			<tbody>
-				@foreach($satker as $data)
-				<tr>
-					<td>{{$data->deputi}}</td>
-					<td>{{$data->departemen}}</td>
-					<td>{{$data->kojk}}</td>
-					<td><label class="btn btn-danger" id="bootBox0">Belum Submit</label></td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
-	<!-- END Datatables Content -->
+           ?>
+           <tbody>
+            @foreach($satker as $data)
+            <tr>
+                <td class="text-center">{{$data->username}}</td>
+                <td class="text-center">{{$data->deputi}}</td>
+                <td class="text-center">{{$data->departemen}}</td>
+                <td class="text-center">{{$data->kojk}}</td>
+                <td class="text-center">
+                    <label class="btn btn-danger" id="bootBox0">Belum Submit</label>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{$satker->render()}}
+</div>
+<!-- END Datatables Content -->
 </div>
 <!-- END Page Content -->
 <!-- END Page Content -->
