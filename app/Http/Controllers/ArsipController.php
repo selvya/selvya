433b65b975f3954383035bc3b49f725b8874b1d3
PetengDedet <pueteng.dedet@gmail.com>
2017-04-03 04:44:17 +0000

    asdasd

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Iku;
 
 class ArsipController extends Controller
 {
     //
     public function arsipInovatifIndex(Request $r)
     {
-
         return view('inovatif.arsip');
     }
 }
