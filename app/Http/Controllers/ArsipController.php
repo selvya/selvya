96268641eb46f0e4ba20843f8e354751972bc71d
ilhamwara <ilhamwara@gmail.com>
2017-04-03 11:18:35 +0700

    as

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Iku;
 
 class ArsipController extends Controller
 {
     //
     public function arsipInovatifIndex(Request $r)
     {
+        // $triwulan = cekCurrentTriwulan();
+        $ikunya = Iku::where('daftarindikator_id','3')
+        ->where('tipe','parameterized')
+        ->where('programbudaya_id','3')
+        ->where('isfinal','y')
+        ->where('satker',Auth::user()->id)
+        ->get();
 
-        return view('inovatif.arsip');
+        return view('inovatif.arsip' , compact('ikunya'));
     }
 }
